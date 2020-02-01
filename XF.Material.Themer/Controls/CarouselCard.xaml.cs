d2f39d7218d9c038d6a64988c8da3441f7c14d85
mjfreelancing <malcolm@mjfreelancing.com>
2020-02-01 14:55:37 +1100

    Replaced the carousel 'card' with a custom control rather than using control templates

---
+++ XF.Material.Themer/Controls/CarouselCard.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms.PancakeView;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Material.Themer.Controls
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class CarouselCard : PancakeView
+  {
+    public CarouselCard()
+    {
+      InitializeComponent();
+    }
+  }
+}
