86b34504bc05cf592efd848650e7e04f8c48b356
mjfreelancing <malcolm@mjfreelancing.com>
2020-02-09 21:36:43 +1100

    Working on theme sample 1

--- XF.Material.Themer/Views/Themes/ThemeView1.xaml.cs
+++ XF.Material.Themer/Views/Themes/ThemeView1.xaml.cs
@@ -1,30 +1,14 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using XF.Material.Themer.Factories;
-using XF.Material.Themer.Models.Themes;
 
 namespace XF.Material.Themer.Views.Themes
 {
   [XamlCompilation(XamlCompilationOptions.Compile)]
   public partial class ThemeView1 : ContentView
   {
-    private readonly IThemeColorsFactory _themeColorsFactory = DependencyService.Resolve<IThemeColorsFactory>();
-
     public ThemeView1()
     {
       InitializeComponent();
-    }
-
-    private void Button_OnLightClicked(object sender, EventArgs e)
-    {
-      CurrentTheme.Instance.ThemeColors = _themeColorsFactory.GetThemeColors(Theme.Light);
-
-    }
-
-    private void Button_OnDarkClicked(object sender, EventArgs e)
-    {
-      CurrentTheme.Instance.ThemeColors = _themeColorsFactory.GetThemeColors(Theme.Dark);
     }
   }
 }
